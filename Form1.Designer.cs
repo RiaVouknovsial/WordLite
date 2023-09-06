f166d7a7ced812003585dc5b7b4336774102c9d5
RiaVouKnovsial <volovik.raisa.kam@gmail.com>
2023-09-06 21:11:20 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WindowsFormsApp18
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(677, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(677, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 37);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(677, 405);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 33);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "отмена";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button9.Image = global::WindowsFormsApp18.Properties.Resources.Photos_31086__3_;
+            this.button9.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button9.Location = new System.Drawing.Point(677, 264);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(111, 36);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "    цвет";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Image = global::WindowsFormsApp18.Properties.Resources.ruler_96684__1_;
+            this.button8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button8.Location = new System.Drawing.Point(677, 194);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(111, 35);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "     размер";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Image = global::WindowsFormsApp18.Properties.Resources.ruler_stationary_triangle_study_geometry_icon_255779__1_;
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.Location = new System.Drawing.Point(677, 334);
+            this.button7.Name = "button7";
+            this.button7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button7.Size = new System.Drawing.Size(111, 39);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "           начертание";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Image = global::WindowsFormsApp18.Properties.Resources.type_font_5496__3_;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(677, 127);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 36);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "шрифт";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(646, 426);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.FontDialog fontDialog1;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+
+            fontDialog1.ShowColor = true;
+            colorDialog1.FullOpen = true;
+        }
+
+        private Stack<TextState> textStateStack = new Stack<TextState>();
+
+        private class TextState
+        {
+            public string Text { get; set; }
+            public Color SelectionColor { get; set; }
+            public Font SelectionFont { get; set; }
+            public FontStyle SelectionFontStyle { get; set; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var currentState = new TextState
+            {
+                Text = richTextBox1.Text,
+                SelectionColor = richTextBox1.SelectionColor,
+                SelectionFont = richTextBox1.SelectionFont,
+                SelectionFontStyle = richTextBox1.SelectionFont.Style
+            };
+            textStateStack.Push(currentState);
+        }
+
+        private void SaveTextState()
+        {
+            var currentState = new TextState
+            {
+                Text = richTextBox1.Text,
+                SelectionColor = Color.FromArgb(richTextBox1.SelectionColor.ToArgb()), // Создаем новый объект Color
+                SelectionFont = new Font(richTextBox1.SelectionFont.FontFamily, richTextBox1.SelectionFont.Size, richTextBox1.SelectionFont.Style), // Создаем новый объект Font
+                SelectionFontStyle = richTextBox1.SelectionFont.Style
+            };
+            textStateStack.Push(currentState);
+        }
+
+//Open
+private void button2_Click(object sender, EventArgs e)
+		{
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            string filename = openFileDialog1.FileName;
+
+            string fileText = System.IO.File.ReadAllText(filename);
+
+            richTextBox1.Text = fileText;
+
+            SaveTextState(); // Сохраняем состояние после загрузки файла
+            MessageBox.Show("File open");
+        }
+        //Save
+		private void button3_Click(object sender, EventArgs e)
+		{
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            string filename = saveFileDialog1.FileName;
+
+            System.IO.File.WriteAllText(filename, richTextBox1.Text);
+
+            SaveTextState(); // Сохраняем состояние после сохранения файла
+            MessageBox.Show("File saved");
+        }
+
+        //Cancel
+		private void button5_Click(object sender, EventArgs e)
+		{
+            if (textStateStack.Count > 0)
+            {
+                var previousState = textStateStack.Pop();
+                richTextBox1.Text = previousState.Text; // Восстанавливаем текст
+                richTextBox1.SelectionStart = 0;
+                richTextBox1.SelectionLength = richTextBox1.Text.Length;
+                richTextBox1.SelectionColor = previousState.SelectionColor; // Восстанавливаем цвет
+                richTextBox1.SelectionFont = new Font(previousState.SelectionFont.FontFamily, previousState.SelectionFont.Size, previousState.SelectionFontStyle); // Восстанавливаем шрифт и начертание
+            }
+        }
+        private void ApplyTextAttributesToSelectedText(Color color, Font font, FontStyle style)
+        {
+            SaveTextState(); // Сохраняем текущее состояние текста в стеке
+            if (richTextBox1.SelectionLength > 0)
+            {
+                int selectionStart = richTextBox1.SelectionStart;
+                int selectionLength = richTextBox1.SelectionLength;
+
+                richTextBox1.SelectionColor = color;
+                richTextBox1.SelectionFont = new Font(font, style); // Устанавливаем цвет и начертание одновременно
+                richTextBox1.Select(selectionStart + selectionLength, 0); // Снимаем выделение
+            }
+        }
+
+        //Font
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+
+            DialogResult result = fontDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                Font selectedFont = fontDialog.Font;
+                ApplyFontToSelectedText(selectedFont);
+            }
+        }
+        //TextStyle
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionLength > 0)
+            {
+                FontStyle newFontStyle = FontStyle.Bold;
+
+                if (richTextBox1.SelectionFont != null && richTextBox1.SelectionFont.Bold)
+                {
+                    newFontStyle = FontStyle.Regular;
+                }
+
+                Font newFont = new Font(
+                    richTextBox1.SelectionFont?.FontFamily,
+                    richTextBox1.SelectionFont?.Size ?? richTextBox1.Font.Size,
+                    newFontStyle);
+
+                ApplyFontStyleToSelectedText(newFontStyle);
+            }
+        }
+        //FontSize
+        private void button8_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+
+            DialogResult result = fontDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                Font selectedFont = fontDialog.Font;
+                ApplyFontToSelectedText(selectedFont);
+            }
+        }
+        //Color
+        private void button9_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+
+            DialogResult result = colorDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                Color selectedColor = colorDialog.Color;
+                ApplyColorToSelectedText(selectedColor);
+            }
+        }
+
+        private void ApplyFontToSelectedText(Font font)
+        {
+            if (richTextBox1.SelectionLength > 0)
+            {
+                int selectionStart = richTextBox1.SelectionStart;
+                int selectionLength = richTextBox1.SelectionLength;
+
+                richTextBox1.SelectionFont = font;
+                richTextBox1.Select(selectionStart + selectionLength, 0); // Снять выделение
+            }
+        }
+
+        private void ApplyFontStyleToSelectedText(FontStyle fontStyle)
+        {
+            if (richTextBox1.SelectionLength > 0)
+            {
+                int selectionStart = richTextBox1.SelectionStart;
+                int selectionLength = richTextBox1.SelectionLength;
+
+                Font currentFont = richTextBox1.SelectionFont;
+                Font newFont = new Font(currentFont, fontStyle);
+
+                richTextBox1.SelectionFont = newFont;
+                richTextBox1.Select(selectionStart + selectionLength, 0); // Снять выделение
+            }
+        }
+
+        private void ApplyColorToSelectedText(Color color)
+        {
+            if (richTextBox1.SelectionLength > 0)
+            {
+                int selectionStart = richTextBox1.SelectionStart;
+                int selectionLength = richTextBox1.SelectionLength;
+
+                richTextBox1.SelectionColor = color;
+                richTextBox1.Select(selectionStart + selectionLength, 0); // Снять выделение
+            }
+        }
+
+    }
+}
